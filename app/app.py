97a9ee6abcee63b342fa7c5c9ac272976c1cee20
SohomPal <sohompal21@gmail.com>
2025-11-14 02:04:56 -0500

    Working on Flask app

--- app/app.py
+++ app/app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from apis.odds_api import OddsAPI
+from db.dynamodb_store_ev_bets import DynamoDBClient
+from services.sharpedge_model import SharpEdge
 import os
 
 app = Flask(__name__)
 
 ODDS_API_KEY = os.getenv("ODDS_API_KEY")
 odds_api = OddsAPI(ODDS_API_KEY)
+
+region = "us-east-2"  # Replace with your AWS region
+table_name = "EV_Bets"
+dynamo_client = DynamoDBClient(region_name=region, table_name=table_name)
 
 @app.route("/")
 def hello_world():
@@ -29,6 +35,32 @@ def nfl_markets():
 @app.route("/nba/markets", methods=["GET"])
 def nba_markets():
     return odds_api.get_nba_all_markets()
+
+@app.route("/storeBet", methods=["PUT"])
+def storeBets():
+    # Extract JSON payload sent by client
+    data = request.get_json()
+
+    if not data:
+        return jsonify({"error": "No JSON body sent"}), 400
+    
+    try:
+        # Pass the data into your Dynamo function
+        result = dynamo_client.store_data(data)
+        
+        return jsonify({
+            "status": "success",
+            "stored": result
+        }), 200
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+# Define the markets we have weights for
+# For each market create an API route to retrieve EV Bets for that market 
+# For each route, you need a function that will create a new instance of SharpEdge and then use the data to generate those bets. You then return those bets back
+
 
 if __name__ == "__main__":
     app.run(debug=True)
